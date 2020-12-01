7450a219218818e51c1f81bedb7caae3a075eb3b
Guanqun Cao <guanqun.cao@volvocars.com>
2020-12-01 22:17:23 +0100

    added a template file

---
+++ tmpl.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from pdb import set_trace
+
+def get_product(arr):
+    print(arr[0]*arr[1]*arr[2])
+
+if __name__ == '__main__':
+    data = pd.read_csv('data/2020_d01_input.txt', header=None).iloc[:, 0].to_numpy()
+    print(triplet)
