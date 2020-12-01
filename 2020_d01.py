108f3e1a48d3d1665660a9eaa54a838d0c8d6696
Guanqun Cao <guanqun.cao@volvocars.com>
2020-12-01 20:39:49 +0100

    add 2020 day 1

---
+++ 2020_d01.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from pdb import set_trace
+
+def find_pair(arr, target):
+    # Assuming only one pair is contained in the array
+    # At least arr containts one value below one half of the target
+    idx = np.argmin(abs(target/2-arr))
+    small_half = arr[:idx]
+    big_half = arr[idx:]
+    pair = []
+    for e in small_half:
+        if target-e in big_half:
+            pair = [e, target-e]
+            break
+    return pair
+
+def find_triplet(arr, target):
+    # At least arr containts one value below one third of the target
+    idx = np.argmin(abs(target/3-arr))
+    small_third = arr[:idx]
+    triplet = []
+    for e in small_third:
+        pair = find_pair(arr, target - e)
+        if len(pair) != 0:
+            pair.append(e)
+            return pair
+
+def get_product(arr):
+    print(arr[0]*arr[1]*arr[2])
+
+if __name__ == '__main__':
+    data = np.sort(pd.read_csv('2020_d01_input.txt', header=None).iloc[:, 0].to_numpy())
+    triplet = find_triplet(data, 2020)
+    get_product(triplet)
+    print(triplet)
