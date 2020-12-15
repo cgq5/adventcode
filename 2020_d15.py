5cb1079469fd976d11637c1116fd1859746515a9
Guanqun Cao <guanqun.cao@volvocars.com>
2020-12-15 10:18:29 +0100

    adde day 15 pt2

--- 2020_d15.py
+++ 2020_d15.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 from copy import deepcopy
+from toolz import memoize, concatv
 from pdb import set_trace
 
 def gen_nums_pt1(init):
     nums = deepcopy(init)
-    for i in range(len(init), 30000000):
+    for i in range(len(init), 2020):
         last = nums[i-1]
         indices = np.where(np.asarray(nums) == last)[0] 
         if len(indices) > 1:
@@ -14,9 +15,18 @@ def gen_nums_pt1(init):
             nums.append(0)
     print(nums[-1])
 
+def get_nums_pt2(current, history, limit=2020):
+    # reversely use value as index and indices as values which is stored in dict 
+    for idx in range(len(history) + 1, limit):
+        prev_idx = history.get(current, 0)
+        history[current] = idx
+        current = idx - prev_idx if prev_idx else 0
+    return current
+
 if __name__ == '__main__':
     data = '18,11,9,0,5,1'
     test = '0,3,6'
-    ins = data 
+    ins = test
     init = [int(n) for n in ins.split(',')] 
-    gen_nums_pt1(init)
+    #print(gen_nums_pt1(init))
+    print(get_nums_pt2(init[-1], {num: idx + 1 for idx, num in enumerate(init[:-1])}, limit=5))
